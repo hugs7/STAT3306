ce33771aff36c19197fd6b4febc39fa6f198d5bb
Hugo Burton <burto732@gmail.com>
2024-09-01 23:22:48 +1000

    Add check for polymorphic

--- quizzes/a1.R
+++ quizzes/a1.R
@@ -5,22 +5,21 @@ loci <- 15
 indiv <- 200
 h2 <- 0.5
 
-
 af <- runif(loci)
 
 x <- t(replicate(indiv, rbinom(loci, 2, af)))
 
-
 # Check polymorphism
-polymorphic = apply(x, 2, var) == 0
-sum(polymorphic)
+polymorphic <- apply(x, 2, var) == 0
+num_polymorphic <- sum(polymorphic)
+if (num_polymorphic != 0) {
+  stop("There are ", num_polymorphic, " monomorphic loci")
+}
 
 x <- scale(x)
 
-
 beta <- rnorm(loci, 0, sqrt(h2/loci))
 genetic <- x %*% beta
-
 
 # Environmental is per person
 environmental <- rnorm(indiv, 0, sqrt(1 - h2))
