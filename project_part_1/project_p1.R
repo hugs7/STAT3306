bd816294ead4e4fa6cd6330da3bfa16c4f12ac46
Hugo Burton <burto732@gmail.com>
2024-09-07 11:56:24 +1000

    Initial commit on project R file

---
+++ project_part_1/project_p1.R
@@ -0,0 +1,6 @@
+# Course: STAT3306: Statistical Analysis of Genetic Data
+# Taken: Semeste 2, 2024
+# Project Part 1
+# Author: Hugo Burton
+# Date: 7th September 2024
+# Due Date: 2pm 04 October 2024
