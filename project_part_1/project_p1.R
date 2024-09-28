eed8e10957a57341503a5dd1dfac5c16d2767317
Hugo Burton <burto732@gmail.com>
2024-09-29 09:43:08 +1000

    Prefix leading dummy digit for index column

--- project_part_1/project_p1.R
+++ project_part_1/project_p1.R
@@ -507,6 +507,11 @@ latex_table <- function(data, out_name, table_align, caption = NULL, col.names =
         logger("TRACE", "Table align with first col hidden: ", quotes(table_align), ".")
     }
 
+    if (is.vector(digits)) {
+        # Prefix leading dummy digit for index column.
+        digits <- c(0, digits)
+    }
+
     table <- xtable(data, align = xtable_table_align, caption = caption, digits = digits)
 
     if (!is.null(col.names)) {
