d2856ecc449818d3f894b6ff72fc6acb01d85528
Hugo Burton <burto732@gmail.com>
2024-09-29 10:07:39 +1000

    Further reduce plot size

--- project_part_1/project_p1.R
+++ project_part_1/project_p1.R
@@ -62,8 +62,8 @@ plots_out_dir <- file.path("./plots")
 plink_out_dir <- file.path("./plink_out")
 out_dir <- file.path("./out")
 
-plot_w <- 700
-plot_h <- 400
+plot_w <- 500
+plot_h <- 300
 
 # Overrides
 overwrite_ext_plots <- FALSE
