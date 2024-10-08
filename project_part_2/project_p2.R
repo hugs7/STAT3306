5594727a6c76b2c9900d68f2424cf21604b4f3f9
Hugo Burton <burto732@gmail.com>
2024-10-08 23:42:07 +1000

    Add project description

--- project_part_2/project_p2.R
+++ project_part_2/project_p2.R
@@ -1,6 +1,6 @@
 # Course: STAT3306: Statistical Analysis of Genetic Data
 # Taken: Semester 2, 2024
-# Project Part 2
+# Project Part 2: Genetic architecture analyses
 # Author: Hugo Burton
 # Student #: s4698512
 # Due Date: 2pm 25 October 2024
