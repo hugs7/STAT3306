0ca0384f5e0e5e3a4ec2fd0845e2d677cf743669
Hugo Burton <burto732@gmail.com>
2024-10-08 23:38:07 +1000

    Initial commit on project part 2 src

---
+++ project_part_2/project_p2.R
@@ -0,0 +1,8 @@
+# Course: STAT3306: Statistical Analysis of Genetic Data
+# Taken: Semester 2, 2024
+# Project Part 2
+# Author: Hugo Burton
+# Student #: s4698512
+# Due Date: 2pm 25 October 2024
+
+
