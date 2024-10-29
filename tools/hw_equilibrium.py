8be59ce88fe4550f82db8bf302dad72fb40b4c1c
Hugo Burton <burto732@gmail.com>
2024-10-29 12:27:52 +1000

    Compute expected counts

--- tools/hw_equilibrium.py
+++ tools/hw_equilibrium.py
@@ -28,7 +28,7 @@ def get_input() -> int:
         return value
 
 
-def calculate_probability(observed: Dict[str, int], total: int, genotype=None) -> Union[float, Tuple[float, float]]:
+def calculate_probability(observed: Dict[str, int], total: int, allele: str) -> float:
     """
     Calculate probability based on observed counts and total.
 
@@ -38,26 +38,50 @@ def calculate_probability(observed: Dict[str, int], total: int, genotype=None) -
     Args:
         observed: observed counts for each genotype
         total: total number of individuals
-        genotype: genotype for which to calculate probability
+        allele: allele to calculate probability for
 
     Returns:
         probability of the given genotype or alleles A and B
     """
-    if genotype is not None:
-        return observed[genotype] / total
+    allele = allele.upper()
+    if len(allele) == 1:
+        homozygous = allele * 2
+        mixed = "AB"
+        return (2 * observed[homozygous] + observed[mixed]) / (2 * total)
+    elif len(allele) == 2:
+        return observed[allele] / total
+
+    logger.error("Invalid allele: %s", allele)
+
+
+def calcualate_expected_count(genotype: str, allele_probabilities: Dict[str, float], total: int) -> float:
+    """
+    Calculate the expected count for a given genotype.
+
+    Args:
+        genotype: genotype to calculate expected count for
+        allele_probabilities: probabilities of alleles A and B
+        total: total number of individuals
+
+    Returns:
+        expected count for the given genotype
+    """
+    allele1, allele2 = genotype
+    if allele1 == allele2:
+        # Homozygous
+        return allele_probabilities[allele1] ** 2 * total
     else:
-        pr_a = (2 * observed["AA"] + observed["AB"]) / (2 * total)
-        pr_b = (2 * observed["BB"] + observed["AB"]) / (2 * total)
-        return pr_a, pr_b
+        # Heterozygous
+        return 2 * allele_probabilities[allele1] * allele_probabilities[allele2] * total
 
 
-def compute_chi_squared(observed: Dict[str, int], expected: Dict[str, float], total: int) -> float:
+def compute_chi_squared(observed: Dict[str, int], expected_counts: Dict[str, float], total: int) -> float:
     """
     Compute the chi-squared test statistic.
 
     Args:
         observed: observed counts for each genotype
-        expected: expected probabilities for each genotype
+        expected_counts: expected probabilities for each genotype
         total: total number of individuals
 
     Returns:
@@ -65,7 +89,7 @@ def compute_chi_squared(observed: Dict[str, int], expected: Dict[str, float], to
     """
     chi_squared = 0
     for genotype in GENOTYPES:
-        expected_count = expected[genotype] * total
+        expected_count = expected_counts[genotype] * total
         chi_squared += (observed[genotype] -
                         expected_count) ** 2 / expected_count
 
@@ -93,8 +117,10 @@ def main():
     genotype_probabilities = {genotype: calculate_probability(
         observed_values, obs_total, genotype) for genotype in GENOTYPES}
 
+    expected_counts = {genotype: calcualate_expected_count(
+        genotype, allele_probabilities, obs_total) for genotype in GENOTYPES}
     chi_squared = compute_chi_squared(
-        observed_values, genotype_probabilities, obs_total)
+        observed_values, expected_counts, obs_total)
 
 
 if __name__ == "__main__":
